e2c48700ea6a3137a547a84d9f56a4555399c1d7
Giovane Menezes <giovane.menezes@live.com>
2020-02-08 17:30:13 -0300

    Business rules

--- QuizMobile/QuizMobile/Sections/Quiz/Quiz/View/QuizViewController.swift
+++ QuizMobile/QuizMobile/Sections/Quiz/Quiz/View/QuizViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class QuizViewController: UIViewController {
     
+    @IBOutlet weak var bottonViewBottonConstraint: NSLayoutConstraint!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var wordTextField: UITextField!
@@ -29,23 +30,85 @@ class QuizViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        hideKeyboardWhenTappedAround()
         setUpTableView()
+        listenObservables()
+        keyboardListener()
         viewModel?.fetchQustion()
+        wordTextField.addTarget(nil, action: #selector(textDidChange), for: .editingChanged)
+    }
+    
+    func keyboardListener() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
     }
     
     func setUpTableView() {
         tableView.register(QuizViewCell.self)
     }
+    
+    func listenObservables() {
+        viewModel?.questionObservable.didChange = { [weak self] response in
+            guard let self = self else { return }
+            switch response {
+            case .loading:
+                print("show loading")
+            case .loaded:
+                self.pointsLabel.text = self.viewModel?.score
+                self.titleLabel.text = self.viewModel?.title
+            case .error(let error):
+                print("Error: \(error)")
+            default:
+                break
+            }
+        }
+        
+        viewModel?.answersObservable.didChange = { [weak self] _ in
+            guard let self = self else { return }
+            self.tableView.reloadData()
+            self.wordTextField.text = ""
+        }
+    }
+    
+    @objc func textDidChange() {
+        if let string = wordTextField.text {
+            viewModel?.answerTextDidUpdate(string: string)
+        }
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            
+            bottonViewBottonConstraint.constant = keyboardHeight
+            view.setNeedsLayout()
+            view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        bottonViewBottonConstraint.constant = 0
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+    }
 }
 
 extension QuizViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return viewModel?.numberOfHits ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return tableView.dequeueReusableCell(of: QuizViewCell.self, for: indexPath) { cell in
-            cell.fill(title: "Passei aqui!!!")
+            cell.fill(title: self.viewModel?.answers[indexPath.row] ?? "")
         }
     }
 }
--- QuizMobile/QuizMobile/Sections/Quiz/Quiz/ViewModel/QuizViewModel.swift
+++ QuizMobile/QuizMobile/Sections/Quiz/Quiz/ViewModel/QuizViewModel.swift
@@ -15,9 +15,45 @@ class QuizViewModel {
         self.service = service
     }
     
+    var questionObservable = Observable<RequestState>(.initialized)
+    var questionModel: Question?
+    var answersObservable = Observable<Bool>(true)
+    var answers = [String]()
+    
+    var score: String {
+        return "\(numberOfHits)/\(questionModel?.answer.count ?? 0)"
+    }
+    var title: String {
+        return questionModel?.question ?? ""
+    }
+    var numberOfHits: Int {
+        return answers.count
+    }
+    
+    func answerTextDidUpdate(string: String) {
+        if validateAnswer(answer: string) && !answers.contains(string) {
+            answers.append(string)
+            answersObservable.value = true
+        }
+    }
+    
+    func validateAnswer(answer: String) -> Bool {
+        return questionModel?.answer.contains(answer) ?? false
+    }
+    
     func fetchQustion() {
-        service.fetchQuestion(1) { (response) in
-            print(response)
+        questionObservable.value = .loading
+        service.fetchQuestion(1) { [weak self] response in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                switch response {
+                case .success(let question):
+                    self.questionModel = question
+                    self.questionObservable.value = .loaded
+                case .failure(let error):
+                    self.questionObservable.value = .error(error)
+                }
+            }
         }
     }
 }
